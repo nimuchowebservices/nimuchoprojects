28a59aefb022ecc8dee097d87baaa172bf30571d
nimuchowebservices <naijukanimlord631@gmail.com>
2026-05-04 06:32:27 -0700

    Remove unnecessary files; ensure proper project structure

--- nimucho_project/urls.py
+++ nimucho_project/urls.py
@@ -1,14 +1,7 @@
-from django.contrib import admin
+﻿from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),  # This includes all URLs from main app
+    path('', include('main.urls')),
 ]
-
-# Add media and static files handling
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
